de49ca7c215d5e1520ff679474eef3f13732c8cc
emil-sxc <emildiscord624@gmail.com>
2026-05-30 21:24:07 +0200

    first bot

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once("clientReady", () => {
+    console.log(`✅ Eingeloggt als ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+
+    if (message.author.bot) return;
+
+    if (message.content === "!ping") {
+        message.reply("🏓 Pong!");
+    }
+
+});
+
+client.login(process.env.TOKEN);
